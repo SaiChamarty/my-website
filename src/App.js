a00d6af65c9e831ddc337e01705d3a6ac419575a
Sai Ganesh Chamarty <123051270+SaiChamarty@users.noreply.github.com>
2025-02-27 14:30:00 -0800

    Added new pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <div className='Text'>
-        <div className='intro'>
-          <h1>Hey there! I'm</h1>
-        </div>
-        <div className='name'>
-          <h1>Sai Chamarty</h1>
-        </div>
-        <div className='description'>
-          <h1>I am an engineer passionate about App development and Machine Learning.</h1>
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar />
+        <div className='Text'>
+          <div className='intro'>
+            <h1>Hey there! I'm</h1>
+          </div>
+          <div className='name'>
+            <h1>Sai Chamarty</h1>
+          </div>
+          <div className='description'>
+            <h1>I am an engineer passionate about App development and Machine Learning.</h1>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
