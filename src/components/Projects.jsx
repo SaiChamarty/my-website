08447533a0ab94468f050fb5cd00415e7cd0333a
Sai Ganesh Chamarty <123051270+SaiChamarty@users.noreply.github.com>
2025-03-23 19:14:06 -0700

    Projects page updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,10 +6,10 @@ function Projects() {
     <div className='App'>
       <div className='Text'>
         <div className='name'>
-          <h1>About me</h1>
+          <h1>Projects</h1>
         </div>
         <div className='description'>
-          <h1>I am an undergrad at UC Davis studying computer science and electrical engineering minor</h1>
+          <h1>Soon to be added!!</h1>
         </div>
       </div>
     </div>
