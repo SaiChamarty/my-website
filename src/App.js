e0b8caa7a1a5744ff13aa27c32041ea07d09240b
Sai Ganesh Chamarty <123051270+SaiChamarty@users.noreply.github.com>
2025-03-26 20:29:29 -0700

    Server Reload error fixed, and 404 error page added, closes #9

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import Saiborg from './components/Saiborg';
 import About from './components/About';
+import Notfound from './components/Notfound'; // Your custom 404 component
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/saiborg" element={<Saiborg />} />
           <Route path="/about" element={<About />} />
+          {/* Catch-all route for unknown paths */}
+          <Route path="*" element={<Notfound />} />
         </Routes>
       </div>
     </BrowserRouter>
