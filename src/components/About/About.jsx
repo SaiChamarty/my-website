609f68f252f8a302bc71d76fb430873541a8ea73
Sai Ganesh Chamarty <123051270+SaiChamarty@users.noreply.github.com>
2025-04-25 21:27:55 -0700

    About css updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,13 @@
-import './About.css';
+import about from './About.module.css';
 
 function About() {
   return (
-    <div className='App'>
-      <div className='Text'>
-        <div className='name'>
+    <div className={about.App}>
+      <div className={about.Text}>
+        <div className={about.name}>
           <h1>About me</h1>
         </div>
-        <div className='description'>
+        <div className={about.description}>
           <h1>I am an undergrad at UC Davis studying computer science and electrical engineering minor</h1>
         </div>
       </div>
